bc1504433a3e1c4beeb4468a51d34ed30e99583d
marinamarshall <marinamarshall2@gmail.com>
2022-02-22 19:13:39 +0000

    Using For Loops Inside HTML

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def about():
     """
     about function
     """
-    return render_template("about.html", page_title="About")
+    return render_template("about.html", page_title="About", list_of_numbers=[1, 2, 3])
 
 
 @app.route("/contact")
