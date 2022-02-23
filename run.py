a996735daac960bc1d4c30686b8afafd0232be28
marinamarshall <marinamarshall2@gmail.com>
2022-02-23 17:34:18 +0000

    POST

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
@@ -38,12 +38,16 @@ def about_member(member_name):
                 member = obj
         return render_template("member.html", member=member)
 
-@app.route("/contact")
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
     """
     contact function
     """
-    return render_template("contact.html", page_title="Contact")    
+    if request.method == "POST":
+        print(request.form.get("name"))
+        print(request.form["email"])
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
